44c01b298269280870ebe515e22cc19dd0d9fbbf
ToniDias <toni.dias.91@gmail.com>
2016-11-23 14:40:49 +0100

    new project structure

---
+++ src/main/java/ch/heig/GamifyApplication.java
@@ -0,0 +1,12 @@
+package ch.heig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamifyApplication.class, args);
+	}
+}
