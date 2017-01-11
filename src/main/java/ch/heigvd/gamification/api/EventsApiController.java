449685c9d4ab81aa1f7aafa9e3229207a7e621fd
Brybry <bryanperroud@hotmail.com>
2017-01-11 14:07:16 +0100

    elastic search implemented

--- src/main/java/ch/heigvd/gamification/api/EventsApiController.java
+++ src/main/java/ch/heigvd/gamification/api/EventsApiController.java
@@ -1,6 +1,21 @@
 package ch.heigvd.gamification.api;
 
+import ch.heigvd.gamification.api.dto.Event;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.nio.entity.NStringEntity;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Date;
+import java.util.TimeZone;
 
 
 @javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringCodegen", date = "2016-12-05T19:25:34.698+01:00")
@@ -8,4 +23,44 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class EventsApiController implements EventsApi {
 
+    private RestClient restClient;
+
+    public EventsApiController() {
+        restClient = RestClient.builder(
+                new HttpHost("evtdb", 9200, "http"),
+                new HttpHost("evtdb", 9300, "http")).build();
+    }
+
+    public int postElastic() {
+        try {
+
+            TimeZone tz = TimeZone.getTimeZone("UTC");
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+            df.setTimeZone(tz);
+            String isoDate = df.format(new Date());
+
+            HttpEntity entity = new NStringEntity(
+                    "{\n" +
+                    "    \"application_id\" : " + 3 + ",\n" +
+                    "    \"type\" : \"" + "Point" + "\",\n" +
+                    "    \"user_id\" : " + 32 + ",\n" +
+                    "    \"post_date\" : \"" + isoDate + "\"\n" +
+                    "}", ContentType.APPLICATION_JSON);
+            Response indexResponse = restClient.performRequest(
+                    "POST",
+                    "/gamification/event",
+                    Collections.<String, String>emptyMap(),
+                    entity);
+
+            return indexResponse.getStatusLine().getStatusCode();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    @Override
+    public ResponseEntity<Event> eventsPost() {
+        return null;
+    }
 }
---
+++ src/main/java/ch/heigvd/gamification/dao/Event.java
@@ -0,0 +1,11 @@
+package ch.heigvd.gamification.dao;
+
+/**
+ * Fichier: <nom>
+ * Auteur: Brybry
+ * Date de création: 14.12.2016
+ * But: <but>
+ */
+public class Event {
+
+}
